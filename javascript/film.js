be69873a7b3126abe840cc3b0d495d8f6168671e
katarina-pen <katarinapenava0711@gmail.com>
2025-04-02 22:03:13 +0200

    Added pages
    I have made 3 additional pages as well as updating the landing page.

---
+++ javascript/film.js
@@ -0,0 +1,61 @@
+const menu = document.querySelector(".menu")
+const hamburger = document.querySelector(".hamburger")
+const closeIcon = document.querySelector(".closeIcon")
+const menuIcon = document.querySelector(".menuIcon")
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach(
+
+    function (menuItem) {
+
+        menuItem.addEventListener("click", toggleMenu);
+
+    }
+
+)
+
+
+// Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
